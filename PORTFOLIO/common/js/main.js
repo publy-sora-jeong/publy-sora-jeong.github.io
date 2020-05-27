612de9547f7fe74f37e42de5b26b5e8bc3733885
SORA JEONG <syriana77@naver.com>
2020-05-27 17:27:03 +0900

    update

--- PORTFOLIO/common/js/main.js
+++ PORTFOLIO/common/js/main.js
@@ -26,10 +26,17 @@ $(function () {
   });
 
 
+
+
   // x: Math.floor(Math.random()) + 150,
   // y: Math.floor(Math.random()) - 400,
 
 
+  $('.menu-btn').on('click', function () {
+    $('#menu').stop().animate({
+      right: '-50%'
+    });
+  });
 
   $(window).on('scroll', function () {
     var scroll = $(this).scrollTop();
@@ -37,9 +44,30 @@ $(function () {
   });
 
   $('#navigation > ul > li').on('mouseenter', function () {
-
     $(this).addClass('on');
   });
+
+
+  var swiper = new Swiper('.works-slide', {
+    slidesPerView: 'auto',
+    speed: 600,
+    loop: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+    centeredSlides: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+
 });
 
 function initDom() {
@@ -57,11 +85,14 @@ function savePosition() {
 }
 
 function activeBtn(scroll) {
-  var base = -100;
+  var base = -300;
   for (var i = 0; i < section_len; i++) {
     if (scroll >= posArray[i] + base && scroll < posArray[i + 1] + base) {
       $section.removeClass('active')
       $section.eq(i).addClass('active');
+
+      $('#navigation li').removeClass('active');
+      $('#navigation li').eq(i).addClass('active');
     }
   }
 }
